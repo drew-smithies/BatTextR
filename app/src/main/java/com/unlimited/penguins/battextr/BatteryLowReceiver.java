fe21b0cb89519d256d79c461c7a6595b60fccde1
Drew <dschevyvette@gmail.com>
2017-02-25 12:55:19 -0600

    Added broadcast receiver with async on receive

---
+++ app/src/main/java/com/unlimited/penguins/battextr/BatteryLowReceiver.java
@@ -0,0 +1,41 @@
+package com.unlimited.penguins.battextr;
+
+import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+
+/**
+ * Created by Drew on 2/25/2017.
+ */
+
+public class BatteryLowReceiver extends BroadcastReceiver {
+    private static final String TAG = "Drew_BatReceiver";
+
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        SQLiteDatabase AlertDatabase = new AlertOpenHelper(context).getWritableDatabase();
+
+        final PendingResult pendingResult = goAsync();
+        AsyncTask<String, Integer, String> asyncTask = new AsyncTask<String, Integer, String>() {
+            @Override
+            protected String doInBackground(String... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("Action: " + intent.getAction() + "\n");
+                sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n");
+                String log = sb.toString();
+                Log.d(TAG, log);
+                // Must call finish() so the BroadcastReceiver can be recycled.
+                pendingResult.finish();
+                return log;
+            }
+        };
+        asyncTask.execute();
+    }
+}
